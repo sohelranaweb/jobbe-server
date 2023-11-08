24b51d26c92870ff800b34cd8891bf029f41e660
sohelranaweb <sohel.rana.web2@gmail.com>
2023-11-08 15:26:05 +0600

    deleted function complete from server side

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(cors());
 app.use(express.json());
-console.log(process.env.BD_USER);
 
 const uri = `mongodb+srv://${process.env.BD_USER}:${process.env.BD_PASS}@cluster0.5rfjgim.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -28,13 +27,9 @@ async function run() {
     const jobCategoriesCollection = client
       .db("jobList")
       .collection("jobCategories");
-    const categoriesCollection = client.db("jobList").collection("categories");
-
-    app.get("/categories", async (req, res) => {
-      const cursor = categoriesCollection.find();
-      const result = await cursor.toArray();
-      res.send(result);
-    });
+    const appliedJobsCollection = client
+      .db("jobList")
+      .collection("appliedJobs");
 
     app.post("/jobCategories", async (req, res) => {
       const newJob = req.body;
@@ -52,7 +47,7 @@ async function run() {
     app.get("/job/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
-      const result = await jobCategoriesCollection.find(query);
+      const result = await jobCategoriesCollection.findOne(query);
       res.send(result);
     });
 
@@ -90,6 +85,28 @@ async function run() {
         job,
         options
       );
+      res.send(result);
+    });
+
+    // Job Delete
+    app.delete("/jobCategories/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await jobCategoriesCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    // applied job related
+    app.get("/appliedJobs", async (req, res) => {
+      const cursor = appliedJobsCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.post("/appliedJobs", async (req, res) => {
+      const newAppliedJob = req.body;
+      console.log(newAppliedJob);
+      const result = await appliedJobsCollection.insertOne(newAppliedJob);
       res.send(result);
     });
 
